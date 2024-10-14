f1c12bb1925e1d2e5f6fb60d064e3d993bd807ce
Marc Steele <marc@steama.co>
2024-10-14 19:25:58 +0100

    Added the base MAUI app

---
+++ NowPlayingClient/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace NowPlayingClient;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
